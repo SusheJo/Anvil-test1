db3242a470c5e890f7bea09667ebdf02f1a42852
zouhar.z@email.cz <zouhar.z@email.cz>
2026-02-28 21:17:20 +0000

    Edited Form1
    Buttons

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,12 +6,18 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         self.init_components(**properties)
 
+    @handle("button_1", "click")
     def button_1_click(self, **event_args):
-        # 1. Zavoláme funkci z vašeho lokálního Pythonu
+        print("Tlačítko bylo stisknuto v prohlížeči")
         # Musí to být uvnitř bloku try/except, abychom viděli, pokud selže spojení
         try:
             vysledek = anvil.server.call('pozdrav_z_windows')
             # 2. Teď je 'vysledek' definován a můžeme ho vypsat
-            alert(vysledek)
+            self.label_1.text = vysledek
         except Exception as e:
             alert(f"Chyba při volání lokálního Pythonu: {e}")
+
+    @handle("button_2", "click")
+    def button_2_click(self, **event_args):
+        print("Tlačítko bylo stisknuto v prohlížeči")
+        pass
