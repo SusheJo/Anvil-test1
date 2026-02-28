85874e1c08df433399f8077d55297fe878a4f5c0
zouhar.z@email.cz <zouhar.z@email.cz>
2026-02-28 21:30:32 +0000

    Edited Form1
    Buttons bez handle

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,18 +6,16 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         self.init_components(**properties)
 
-    @handle("button_1", "click")
+    # ŽÁDNÉ @handle! Anvil si to pohlídá sám.
     def button_1_click(self, **event_args):
         print("Tlačítko bylo stisknuto v prohlížeči")
-        # Musí to být uvnitř bloku try/except, abychom viděli, pokud selže spojení
         try:
+            # Voláme funkci 'pozdrav_z_windows' ve vašem PC
             vysledek = anvil.server.call('pozdrav_z_windows')
-            # 2. Teď je 'vysledek' definován a můžeme ho vypsat
             self.label_1.text = vysledek
         except Exception as e:
             alert(f"Chyba při volání lokálního Pythonu: {e}")
 
-    @handle("button_2", "click")
     def button_2_click(self, **event_args):
-        print("Tlačítko bylo stisknuto v prohlížeči")
+        print("Tlačítko 2 bylo stisknuto")
         pass
