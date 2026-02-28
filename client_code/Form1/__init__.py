39c6891e9a8d04777ee89bd13dd7cfc5054d1e3f
zouhar.z@email.cz <zouhar.z@email.cz>
2026-02-28 19:53:40 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,19 +1,12 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
-  @handle("button_1", "clicked")
-  def button_1_click(self, **event_args):
-      """This method is called when the button is clicked"""
-      pass
-
-  @handle("text_box_1", "pressed_enter")
-  def text_box_1_pressed_enter(self, **event_args):
-      """This method is called when the user presses Enter in this text box"""
-      pass
+    def button_1_click(self, **event_args):
+        # Volání funkce, která běží u vás v Dockeru/lokálně
+        vysledek = anvil.server.call('pozdrav_z_lokalu', 'Uživateli')
+        self.label_1.text = vysledek
